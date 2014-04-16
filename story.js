b788b76b7178230520a5d6f11f7105de8f19ce31
sigon <mapping.and.co@gmail.com>
2014-04-16 19:42:18 +0200

    change basemap from cloudmade to openstreetmap.org

--- story.js
+++ story.js
@@ -1,10 +1,9 @@
-
 function Map() {
     var map = L.map('map').setView([36.7, -2.4], 8);
 
-    L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: " &copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors",
+      maxZoom: 18
     }).addTo(map);
 
     //L.marker([51.5, -0.09]).addTo(map)
